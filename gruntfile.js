74a12b5d3b136b34a704a3fd490ac8d56c774bcd
Rumman Amin <Rumman.amin10@gmail.com>
2013-12-23 23:05:51 +0000

    fixed grunt imagemin code

--- gruntfile.js
+++ gruntfile.js
@@ -9,16 +9,16 @@ module.exports = function(grunt) {
       my_target: {
         files: {
           'js/main.js': ['components/js/plugins.js', 'components/js/main.js']
-        } //files
-      } //my_target
-    }, //uglify
+        } 
+      } 
+    }, 
     compass: {
       dev: {
         options: {
           config: 'config.rb'
-        } //options
-      } //dev
-    }, //compass
+        } 
+      } 
+    }, 
     imagemin: {
       dynamic: {
         files: [{
@@ -34,22 +34,22 @@ module.exports = function(grunt) {
       scripts: {
         files: ['components/js/*.js'],
         tasks: ['uglify']
-      }, //script
+      }, 
       sass: {
         files: ['components/sass/*.scss'],
         tasks: ['compass:dev']
-      }, //sass
+      }, 
       html: {
         files: ['*.html']
-      }, //html
+      }, 
       images: {
         files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
         tasks: ['imagemin'],
         options: {
-          spawn: false,
+          spawn: false
         }
-      } //images
-    } //watch
-  }) //initConfig
+      } 
+    } 
+  }) 
   grunt.registerTask('default', 'watch');
-} //exports
+} 
