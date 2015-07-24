a09b18ee64e7430797642c758a22870696ea8489
Rumman Amin <Rumman.amin10@gmail.com>
2015-07-24 22:35:54 +0100

    Update grunt/git

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+  'use strict';
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
@@ -21,30 +23,36 @@ module.exports = function(grunt) {
     }, 
     autoprefixer: {
       options: {
-        browsers: ['last 2 version', 'ie 8', 'ie 9']
+        browsers: ['last 5 versions']
       },
-      single_file: {
-        src: 'css/style.css',
-        dest: 'css/style.css'
+      dist: {
+        files: {
+          'style.css': 'style.css'
+        }
       }
     },
     watch: {
-      options: { livereload: true },
       scripts: {
         files: ['components/js/*.js'],
         tasks: ['uglify']
       }, 
       sass: {
-        files: ['components/sass/*.scss'],
-        tasks: ['compass:dev']
+        files: ['components/sass/*.scss']
       }, 
       html: {
         files: ['*.html']
       }, 
       styles: {
-          files: ['css/style.css'],
-          tasks: ['autoprefixer']
-      }
+          files: ['css/style.css']
+      },
+      compass: {
+        files: ['components/sass/*.scss'],
+        tasks: ['compass:dev', 'autoprefixer']
+      },
+      livereload: {
+        options: { livereload: true },
+        files: ['css/style.css']
+      } 
     } 
   }) 
   grunt.registerTask('default', 'watch');
